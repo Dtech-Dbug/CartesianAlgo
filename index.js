43b4bc872b7c841c689f5db967bd29a7985848b9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-20 17:53:27 +0530

    index.js : transpiled javascript from ts

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var A = [0, 1, 2];
 var B = [1, 2, 3];
-console.log(A);
+// let array = [];
+var cartesianProduct = function (setA, setB) {
+    var i;
+    var j;
+    var list = [];
+    for (i = 0; i < setA.length; i++) {
+        for (j = 0; j < setB.length; j++) {
+            list.push([setA[i], setB[j]]);
+        }
+    }
+    console.log(list);
+};
+console.log(cartesianProduct(A, B));
