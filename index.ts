81dc6168a393663f60e1531f85b17da2b65c794f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-20 17:53:09 +0530

    update : index.ts
    - wrote codes for finding cross product of two sets

--- index.ts
+++ index.ts
@@ -4,4 +4,19 @@ type set = [digits, digits, digits];
 let A: set = [0, 1, 2];
 let B: set = [1, 2, 3];
 
-console.log(A);
+// let array = [];
+
+let cartesianProduct = (setA: set, setB: set) => {
+  let i: digits;
+  let j: digits;
+  let list = [];
+
+  for (i = 0; i < setA.length; i++) {
+    for (j = 0; j < setB.length; j++) {
+      list.push([setA[i], setB[j]]);
+    }
+  }
+  console.log(list);
+};
+
+console.log(cartesianProduct(A, B));
