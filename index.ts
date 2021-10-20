a4b939e2ccf7c77554adc321081b8e036ef89d49
Dwaipayan C(Dtech-Dbug) <heydwaipayanhere@gmail.com>
2021-10-20 13:10:49 +0530

    update :  removed redundant comments

--- index.ts
+++ index.ts
@@ -15,23 +15,26 @@ let cartesianProduct = (setA: set, setB: set) => {
   let relatedList: set = [];
   let unRelatedList: set = [];
 
-  // console.log(typeof setA[1]);
-  // console.log(JSON.stringify(setA));
-  // console.log(setA);
-
+ // find the set of ordered pairs by concat. each items from first set to each items from the second
   for (i = 0; i < setA.length; i++) {
     for (j = 0; j < setB.length; j++) {
       orderedPairs.push([setA[i], setB[j]]);
     }
   }
-  orderedPairs.forEach((pair, index) => {
-    // console.log(pair);
+  
+  // loop thru the set of ordered pairs and do something
 
+  orderedPairs.forEach((pair, index) => {
+ 
+   // check for relations that abide by the A[i] < B[j] relation
     pair.reduce((prev, next) => {
       if (prev < next) {
+        //store the relations in a set
         relatedList.push(pair);
         console.log(relatedList);
       } else {
+        
+        //store the products that are not related seperately
         unRelatedList.push(pair);
         console.log("unrelated list-->", unRelatedList);
       }
